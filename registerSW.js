ee2317c3f790859acfed371a41408ce9d9d9cffb
Jairo-GitHub-Principal <jairocesa@outlook.com.br>
2025-02-03 21:56:35 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/aluraFlix_Pwa/sw.js', { scope: '/aluraFlix_Pwa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
